3ee06e93a50dd7e23822e06332589b654a6e7922
itzzmegrrr <itzzmegrrr@gmail.com>
2023-03-18 14:02:14 +0530

    Initial Commit

---
+++ getUID.py
@@ -0,0 +1,54 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description="Get UID from Gmail")
+parser.add_argument("-e", "--email", help="Email", required=True)
+args = parser.parse_args()._get_kwargs()
+
+email = args[0][1]
+
+
+def load_headers():
+    with open("request.txt", "r") as f:
+        raw_request = f.read()
+
+    # Split the raw request into its component parts
+    request_lines = raw_request.split("\n")
+    headers = {}
+    for line in request_lines[1:]:
+        if line == "":
+            break
+        key, value = line.split(": ")
+        headers[key] = value
+    return headers
+
+
+session = requests.Session()
+
+url = "https://people-pa.clients6.google.com/$rpc/google.internal.people.v2.minimal.PeopleApiAutocompleteMinimalService/ListAutocompletions"
+
+headers = load_headers()
+
+data = [
+    f"{email}",
+    None,
+    None,
+    ["GMAIL_COMPOSE_WEB_POPULOUS"],
+    8,
+    None,
+    None,
+    None,
+    ["GMAIL_COMPOSE_WEB_POPULOUS", None, 2],
+]
+
+
+res = session.post(
+    url,
+    headers=headers,
+    json=data,
+)
+
+parsed_data = res.json()
+uid = parsed_data[0][0][3][0]
+image_url = parsed_data[0][0][3][3][0][1]
+print(f"""{{"UID":"{uid}","image_url":"{image_url}"}}""")
